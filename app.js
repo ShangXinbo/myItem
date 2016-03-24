2f206146279f8a1df219010f3334470a14b72382
shangxinbo@geotmt.com <shangxinbo@geotmt.com>
2016-03-24 17:29:14 +0800

    上传文件

--- app.js
+++ app.js
@@ -22,16 +22,24 @@ app
 .get('/upload',function(req,res){
     res.render('index',{title:"shang",message:'点我上传'});
 })
-.post('/upload/submit', upload.single('file'), function(req,res){
-    //console.log(req.body);
-    fs.open(req.file.originalname,'w','0644',function(e,fd){
+.post('/upload/submit', upload.single('file'), function(req,res){  
+    console.log(req.body);
+    console.log(req.file);
+    /*fs.open(req.file.originalname,'w','0644',function(e,fd){
         if(e) throw e;
-        fs.write(fd,req.file,function(e){
-            if(e) throw e;
-            fs.closeSync(fd);
-        });
-    });
-
+        fs.read(req.file.path,function(err,data){
+            if(err){
+                console.log(err);
+            }else{ 
+                console.log(data.length);
+            }
+            fs.write(fd,data,function(e){
+                if(e) throw e;
+                fs.closeSync(fd);
+            });
+        })
+        
+    });*/
     res.end();
 })
 
--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,8 @@
 $(function(){
     $('#shang').xUpload({
         url : '/upload/submit/',
-        data:{
-            shang:'asdfa'
-        },
         //accept: 'application/pdf',
-        maxSize: 4000000000000000,
+        maxSize: 4000000000,
         onError: function(error){
             console.log(error);
         },
