13a13cd757bd95f6e7fc0650ada66f36d0c9ba9a
shangxinbo@geotmt.com <shangxinbo@geotmt.com>
2016-04-13 16:25:26 +0800

    添加404页

--- configs/routes.js
+++ configs/routes.js
@@ -6,44 +6,24 @@ const sms = require('../controllers/sms');
 const index = require('../controllers/index');
 const account = require('../controllers/account');
 
-
 module.exports = function (app) {
     app
         .get('/', index)
-        .post('/upload/submit', uploads.single('file'), upload.submit)
-        .get('/upload/', upload.showtpl)
+        .post('/upload/submit',account.isLogged, uploads.single('file'), upload.submit)
+        .get('/upload/',account.isLogged, upload.showtpl)
         .get('/sms/sendSingle', account.isLogged, sms.sendSingle)
         .get('/sms', account.isLogged, sms.showtpl)
         .all('/login*', account.login)
         .all('/logout', account.logout);
 
-    app.use(function (err, req, res, next) {
+    app.get('*',function(req,res){
+        res.render('404');
+    });
 
-        if (err.message && (~err.message.indexOf('not found') || (~err.message.indexOf('Cast to ObjectId failed')))) {
-            return next();
-        }
-
-        console.error(err.stack);
-
-        if (err.stack.includes('ValidationError')) {
-            res.status(422).render('422', {
-                error: err.stack
-            });
-            return;
-        }
-
-        // error page
+    app.use(function (err, req, res) {
         res.status(500).render('500', {
             error: err.stack
         });
-    });
-
-    // assume 404 since no middleware responded
-    app.use(function (req, res) {
-        /*res.status(404).render('404', {
-         url: req.originalUrl,
-         error: 'Not found'
-         });*/
     });
 };
 
