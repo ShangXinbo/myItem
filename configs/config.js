369166ad680f5ab127de0dbd9efa713d4d839989
shangxinbo@geotmt.com <shangxinbo@geotmt.com>
2016-05-05 13:02:03 +0800

    no message

--- configs/config.js
+++ configs/config.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = {
-    sms_apikey : '80b1',   //短信平台的apikey
+    sms_apikey : '80b19200e90dcc958506a48fea5387eb',   //短信平台的apikey
     sms_host : 'sms.yunpian.com',
     sms_port : 443
 };
