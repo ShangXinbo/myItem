0869a3ddaddf148577c04293d02f79c9980f0cfd
Xinbo Shang <314911714@qq.com>
2016-05-09 19:39:39 +0800

    Update config.js

--- configs/config.js
+++ configs/config.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = {
-    sms_apikey : '80b19200e90dcc958506a48fea5387eb',      //短信平台的apikey,需要去云片网络注册
+    sms_apikey : '',      //短信平台的apikey,需要去云片网络注册
     sms_host : 'sms.yunpian.com',
     sms_port : 443
 };
